01376c5161f6c26cd51ec43eb27785908b77ae5e
Kiss Máté Tamás <matekiss343@gmail.com>
2024-06-17 21:44:55 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -271,7 +271,7 @@
     portal = {
       enable = true;
       config.common.default = "*";
-      extraPortals = [pkgs.xdg-desktop-portal-kde pkgs.xdg-desktop-portal-gtk];
+      extraPortals = [pkgs.xdg-desktop-portal-kde pkgs.xdg-desktop-portal-gtk xdg-desktop-portal-hyprland];
     };
   };
   #Sudo
