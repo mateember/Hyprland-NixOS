ce8aec2f6ea63b63673016dd8ce745b1bf47aee3
... <...>
2024-06-17 14:46:10 +0200

    separate package fiel

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./packages.nix
   ];
 
   hardware = {
@@ -44,6 +45,7 @@
     };
     extraOptions = ''
       experimental-features = nix-command flakes
+      experimental-features = nix-command
       keep-outputs = true
       keep-derivations = true
     '';
@@ -227,59 +229,6 @@
       }
     ];
   };
-
-  chaotic.mesa-git.enable = true;
-  # System packages
-  nixpkgs.config.allowUnfree = true;
-  #pkgs-unstable.config.allowUnfree = true;
-  environment.systemPackages =
-    (with pkgs; [
-      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      wget
-      nano
-      firefox
-      tree
-      starship
-      zoxide
-      (callPackage ./customPackages/sddm-bluish.nix {}).sddm-bluish
-      (callPackage ./customPackages/sddm-andromeda.nix {}).sddm-andromeda
-      #(callPackage ./customPackages/HyprlandDesktop/default.nix { }).hyprland-desktop
-      kdePackages.qtstyleplugin-kvantum
-      kdePackages.kdecoration
-      fuse
-      sshfs-fuse
-      killall
-      btop
-      fzf
-      podman-tui
-      dive
-      podman-compose
-      kdePackages.extra-cmake-modules
-      pavucontrol
-      sddm-kcm
-
-      #Development packages
-      llvm
-      clang
-      git
-      cmake
-      gcc
-      gnumake
-      flex
-      bison
-      lld
-      elfutils
-      nix-prefetch-git
-    ])
-    ++ (with pkgs-unstable; [
-      bat
-      lact
-      kdePackages.polkit-kde-agent-1
-      kdePackages.kirigami
-      polkit_gnome
-      kitty
-      alacritty
-    ]);
 
   virtualisation = {
     libvirtd.enable = true;
---
+++ packages.nix
@@ -0,0 +1,65 @@
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-unstable,
+  hyprland,
+  ...
+}: {
+
+  #Chaotic AUR
+  chaotic.mesa-git.enable = true;
+
+
+
+  # System packages
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages =
+    (with pkgs; [
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+      nano
+      firefox
+      tree
+      starship
+      zoxide
+      (callPackage ./customPackages/sddm-bluish.nix {}).sddm-bluish
+      (callPackage ./customPackages/sddm-andromeda.nix {}).sddm-andromeda
+      #(callPackage ./customPackages/HyprlandDesktop/default.nix { }).hyprland-desktop
+      kdePackages.qtstyleplugin-kvantum
+      kdePackages.kdecoration
+      fuse
+      sshfs-fuse
+      killall
+      btop
+      fzf
+      podman-tui
+      dive
+      podman-compose
+      kdePackages.extra-cmake-modules
+      pavucontrol
+      sddm-kcm
+
+      #Development packages
+      llvm
+      clang
+      git
+      cmake
+      gcc
+      gnumake
+      flex
+      bison
+      lld
+      elfutils
+      nix-prefetch-git
+    ])
+    ++ (with pkgs-unstable; [
+      bat
+      lact
+      kdePackages.polkit-kde-agent-1
+      kdePackages.kirigami
+      polkit_gnome
+      kitty
+      alacritty
+    ]);
+}
