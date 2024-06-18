6c08435ec6c8d2e0a97a5ca3957f5c667a86b77a
Kiss Máté Tamás <matekiss343@gmail.com>
2024-06-18 21:24:25 +0200

    Update default.nix

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -2,6 +2,6 @@
   xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
   xdg.configFile."waybar/modules.jsonc".source = ./modules.jsonc;
   xdg.configFile."waybar/style.css".source = ./style.css;
-  xdg.configFile."waybar/config-sway.jsonc".source = ./config.jsonc-sway;
+  xdg.configFile."waybar/config-sway.jsonc".source = ./config-sway.jsonc;
   xdg.configFile."waybar/style-sway.css".source = ./style-sway.css;
 }
