8f2ee37c2e9c92add94c79cd3fdfbe1a5f44417b
Kiss Máté Tamás <matekiss343@gmail.com>
2024-06-18 21:02:28 +0200

    home manager

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -15,13 +15,42 @@
   };
 
 
-  xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
+  hyprland.extraConfig = '' 
+  
+  
+#monitor=HDMI-A-1,1920x1080@144,0x0,1,vrr,2,bitdepth,10
+monitor=DP-3,1920x1080@75,0x0,1,vrr,2
+
+source = ~/.config/hypr/conf/keyboard.conf
+source = ~/.config/hypr/conf/windowrules.conf
+source = ~/.config/hypr/conf/autostart.conf
+source = ~/.config/hypr/conf/looks.conf
+source = ~/.config/hypr/conf/environment.conf
+# See https://wiki.hyprland.org/Configuring/Keywords/ for more
+
+
+#workspace=1,monitor:HDMI-A-1,default:true
+b
+
+
+
+
+
+
+  
+  
+  '';
+
+
+
+
+
   xdg.configFile."hypr/conf/environment.conf".source = ./conf/environment.conf;
   xdg.configFile."hypr/conf/autostart.conf".source = ./conf/autostart.conf;
   xdg.configFile."hypr/conf/keyboard.conf".source = ./conf/keyboard.conf;
   xdg.configFile."hypr/conf/looks.conf".source = ./conf/looks.conf;
   xdg.configFile."hypr/conf/windowrules.conf".source = ./conf/windowrules.conf;
-  xdg.configFile."hypr/hyprestart.sh.conf".source = ./hyprestart.conf;
+  xdg.configFile."hypr/hyprestart.sh".source = ./hyprestart.sh;
   xdg.configFile."hypr/hypridle.conf".source = ./hypridle.conf;
   xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
   xdg.configFile."hypr/hyprpaper.conf".source = ./hyprpaper.conf;
